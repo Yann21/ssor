6c2d95e3bdc394f91e6909e3cbe6e26bfa892f15
Yann Hoffmann <yann.hoffmaaann@gmail.com>
2024-02-06 11:08:49 +0100

    Update config

--- ssor/config.py
+++ ssor/config.py
@@ -1,9 +1,9 @@
 import os
-from dotenv import dotenv
+from dotenv import load_dotenv
 
-dotenv()
+load_dotenv()
 
 org_roam_path = "/home/yann/Documents/Application/KnowledgeManager/pages"
-persist_directory = "/home/yann/persist"
+persist_directory = "/home/yann/Documents/Application/KnowledgeManager/chroma"
 banned_org_roam_files = {"screening"}
 openai_api_key = os.environ["OPENAI_API_KEY"]
